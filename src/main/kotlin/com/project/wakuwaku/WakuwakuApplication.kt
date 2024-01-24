5149925ac23c10ba8637e2fd2e1215fbd17ce98a
oob00 <oob00@naver.com>
2024-01-24 14:27:34 +0900

    프로젝트 연결 첫커밋

---
+++ src/main/kotlin/com/project/wakuwaku/WakuwakuApplication.kt
@@ -0,0 +1,11 @@
+package com.project.wakuwaku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WakuwakuApplication
+
+fun main(args: Array<String>) {
+	runApplication<WakuwakuApplication>(*args)
+}
