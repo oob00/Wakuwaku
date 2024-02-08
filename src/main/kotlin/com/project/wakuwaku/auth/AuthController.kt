d2af886929fb86566bfd3443ab7989bea4992732
HeroKim <ghnm89@naver.com>
2024-02-08 13:56:07 +0900

    JWT 설정 추가 및 로그인 API 작성 (누락본 추가)

--- src/main/kotlin/com/project/wakuwaku/auth/AuthController.kt
+++ src/main/kotlin/com/project/wakuwaku/auth/AuthController.kt
@@ -1,18 +1,24 @@
 package com.project.wakuwaku.auth
 
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import com.project.wakuwaku.auth.dto.LoginDto
+import com.project.wakuwaku.config.auth.JwtInfo
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class AuthController {
+@RequestMapping("/api/auth")
+@RestController
+class AuthController(
+    private val authService: AuthService
+) {
 
-    @GetMapping("/index")
-    fun home(): String {
-        return "index"
+    @PostMapping("/login")
+    fun login(@RequestBody loginDto: LoginDto): ResponseEntity<JwtInfo> {
+        val token = authService.login(loginDto)
+
+        return ResponseEntity.ok(token)
     }
 
-    @GetMapping("/login")
-    fun login(): String {
-        return "login"
-    }
 }
--- src/main/kotlin/com/project/wakuwaku/auth/AuthService.kt
+++ src/main/kotlin/com/project/wakuwaku/auth/AuthService.kt
@@ -1,29 +1,16 @@
 package com.project.wakuwaku.auth
 
-import com.project.wakuwaku.model.jpa.user.UserRepository
-import com.project.wakuwaku.model.jpa.user.Users
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
+import com.project.wakuwaku.auth.dto.LoginDto
+import com.project.wakuwaku.config.auth.JwtInfo
+import com.project.wakuwaku.config.auth.JwtUtil
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService(
-    private val userRepository: UserRepository
-): UserDetailsService {
+    private val jwtUtil: JwtUtil
+) {
 
-    /**
-     * spring security 에서 db의 회원 정보를 조회해서 로그인 시도한 회원의 정보가 있는지 확인
-     */
-    override fun loadUserByUsername(userId: String): UserDetails {
-        val user: Users = userRepository.findById(userId)
-            .orElseThrow { UsernameNotFoundException("존재하지 않는 회원입니다.") }
-
-        return User.builder()
-            .username(user.id)
-            .password(user.password)
-            .build()
+    fun login(dto: LoginDto): JwtInfo {
+        return jwtUtil.createJwt(dto.id)
     }
-
 }
