7aa408a9c6c1e7c3cfca4176ca625ff6c4f1f909
oob00 <oob00@naver.com>
2024-02-28 01:22:19 +0900

    git action 설정 변경

--- src/test/kotlin/com/project/wakuwaku/chat/ChatTest.kt
+++ src/test/kotlin/com/project/wakuwaku/chat/ChatTest.kt
@@ -46,8 +46,7 @@ import java.util.concurrent.TimeUnit
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext
-@EmbeddedKafka(partitions = 1, topics = [KafkaConstants.KAFKA_TOPIC])
+@EmbeddedKafka(partitions = 2, topics = [KafkaConstants.KAFKA_TOPIC])
 class ChatTest @Autowired constructor(
         private val userRepository: UserRepository,
         private val jwtUtil: JwtUtil
