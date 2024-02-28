8894d033e1c89f2bfc36bb91a7778a9ca6dc30a8
HeroKim <ghnm89@naver.com>
2024-02-28 18:07:20 +0900

    테스트 코드 오류 수정
    - JPA Auditing Config 분리

--- src/main/kotlin/com/project/wakuwaku/WakuwakuApplication.kt
+++ src/main/kotlin/com/project/wakuwaku/WakuwakuApplication.kt
@@ -2,9 +2,7 @@ package com.project.wakuwaku
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@EnableJpaAuditing
 @SpringBootApplication
 class WakuwakuApplication
 
